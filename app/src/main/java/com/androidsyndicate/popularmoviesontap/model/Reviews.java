038039e98e1813f6cbf077d092c698ebc32048ce
Erick Shaffer <erick.shaffer321@gmail.com>
2017-05-06 16:40:16 -0700

    Updated models to reduce number of classes

---
+++ app/src/main/java/com/androidsyndicate/popularmoviesontap/model/Reviews.java
@@ -0,0 +1,142 @@
+package com.androidsyndicate.popularmoviesontap.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reviews implements Parcelable {
+
+
+    private int id;
+    private int page;
+    private int total_pages;
+    private int total_results;
+    private List<ReviewBean> results;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    public List<ReviewBean> getResults() {
+        return results;
+    }
+
+    public void setResults(List<ReviewBean> results) {
+        this.results = results;
+    }
+
+    public static class ReviewBean {
+
+        private String id;
+        private String author;
+        private String content;
+        private String url;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+    protected Reviews(Parcel in) {
+        id = in.readInt();
+        page = in.readInt();
+        total_pages = in.readInt();
+        total_results = in.readInt();
+        if (in.readByte() == 0x01) {
+            results = new ArrayList<ReviewBean>();
+            in.readList(results, ReviewBean.class.getClassLoader());
+        } else {
+            results = null;
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeInt(page);
+        dest.writeInt(total_pages);
+        dest.writeInt(total_results);
+        if (results == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(results);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Reviews> CREATOR = new Parcelable.Creator<Reviews>() {
+        @Override
+        public Reviews createFromParcel(Parcel in) {
+            return new Reviews(in);
+        }
+
+        @Override
+        public Reviews[] newArray(int size) {
+            return new Reviews[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/androidsyndicate/popularmoviesontap/model/Videos.java
@@ -0,0 +1,145 @@
+package com.androidsyndicate.popularmoviesontap.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Videos implements Parcelable {
+
+    private int id;
+    private List<VideoBean> results;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<VideoBean> getResults() {
+        return results;
+    }
+
+    public void setResults(List<VideoBean> results) {
+        this.results = results;
+    }
+
+    public static class VideoBean {
+
+
+        private String id;
+        private String iso_639_1;
+        private String iso_3166_1;
+        private String key;
+        private String name;
+        private String site;
+        private int size;
+        private String type;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getIso_639_1() {
+            return iso_639_1;
+        }
+
+        public void setIso_639_1(String iso_639_1) {
+            this.iso_639_1 = iso_639_1;
+        }
+
+        public String getIso_3166_1() {
+            return iso_3166_1;
+        }
+
+        public void setIso_3166_1(String iso_3166_1) {
+            this.iso_3166_1 = iso_3166_1;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSite() {
+            return site;
+        }
+
+        public void setSite(String site) {
+            this.site = site;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+    }
+
+    protected Videos(Parcel in) {
+        id = in.readInt();
+        if (in.readByte() == 0x01) {
+            results = new ArrayList<VideoBean>();
+            in.readList(results, VideoBean.class.getClassLoader());
+        } else {
+            results = null;
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        if (results == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(results);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Videos> CREATOR = new Parcelable.Creator<Videos>() {
+        @Override
+        public Videos createFromParcel(Parcel in) {
+            return new Videos(in);
+        }
+
+        @Override
+        public Videos[] newArray(int size) {
+            return new Videos[size];
+        }
+    };
+}
